8ccc2b14fc4de34ca68ee465cb893b638bdf73c8
Tanilse <tanilsec@gmail.com>
2026-01-21 14:00:14 -0500

    Creacion de empleado y gerente

--- Mision1/Proyecto1/src/com/empresa/gestion/model/Empleado.java
+++ Mision1/Proyecto1/src/com/empresa/gestion/model/Empleado.java
@@ -1,5 +1,24 @@
 package com.empresa.gestion.model;
 
 public class Empleado extends Persona {
-    
+    private double salario;
+
+    public Empleado(String nombre, int edad, double salario){
+        super(nombre,edad);
+        this.salario = salario;
+    }
+
+    public double getSalario(){
+        return salario;
+    }
+
+    public void setSalario(double salario){
+        this.salario = salario;
+    }
+
+    //♻️Sobreescritura de metodo, Polimorfismo
+    @Override
+    public void presentarse(){
+        System.out.println("Soy el empleado "+getNombre()+"y mi salario es "+salario);
+    }
 }
